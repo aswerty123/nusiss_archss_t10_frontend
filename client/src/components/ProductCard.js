08df0114ec11b057348aacb2e90b0cabecea4d74
gohpeijin <gohpeijin@gmail.com>
2023-10-08 23:27:39 +0800

    modify image type passing to backend

--- client/src/components/ProductCard.js
+++ client/src/components/ProductCard.js
@@ -1,35 +1,45 @@
+import { Buffer } from 'buffer';
 import React, { useEffect, useState } from 'react';
 import { useDeleteProductMutation } from '../queries/product-queries';
 import { CgArrowsExchange } from 'react-icons/cg';
 import { AiFillHeart, AiOutlineHeart } from 'react-icons/ai';
 
-import { BsFillTrashFill} from 'react-icons/bs'
+import { BsFillTrashFill } from 'react-icons/bs';
 
 /** @jsxImportSource @emotion/react */
 import tw, { styled } from 'twin.macro';
 import { useAuth } from '../context/AuthContext';
-import { useAddToWishlistMutation, useCartQuery, useDeleteFromWishlistMutation, useWishlistQuery } from '../queries/shopping-queries';
+import {
+  useAddToWishlistMutation,
+  useCartQuery,
+  useDeleteFromWishlistMutation,
+  useWishlistQuery,
+} from '../queries/shopping-queries';
 
 const CardContainer = tw.div`border border-gray-200 rounded-lg overflow-hidden relative`;
-const CardImage = tw.div`w-full h-32 bg-center bg-no-repeat relative`
-const CardButton = tw.button`text-white px-3 py-1.5 text-sm font-semibold rounded focus:outline-none`
+const CardImage = tw.div`w-full h-32 bg-center bg-no-repeat relative`;
+const CardButton = tw.button`text-white px-3 py-1.5 text-sm font-semibold rounded focus:outline-none`;
 const AvailabilityTag = tw.div`absolute top-2 left-2 bg-green-100 border-2 border-green-400 text-green-700 rounded p-1 text-xs`;
 const OutOfStockTag = tw.div`absolute top-2 left-2 bg-red-100 border-2 border-red-400 text-red-700 rounded p-1 text-xs`;
-const HeartIcon = tw.div`absolute top-2 right-2 p-2`; 
-const ImageSwitchIconContainer = tw.div`absolute bottom-2 right-2 text-white bg-black bg-opacity-50`
-const CardContentContainer = tw.div`p-4 relative`
+const HeartIcon = tw.div`absolute top-2 right-2 p-2`;
+const ImageSwitchIconContainer = tw.div`absolute bottom-2 right-2 text-white bg-black bg-opacity-50`;
+const CardContentContainer = tw.div`p-4 relative`;
 
-export const ProductCard = ({ product, wishlistIdArray}) => {
-    const { authData } = useAuth();
+export const ProductCard = ({ product, wishlistIdArray }) => {
+  const { authData } = useAuth();
   const [imageFormat, setImageFormat] = useState(true);
-  
+
   // const cartQuery = useCartQuery();
   // const wishlistQuery = useWishlistQuery();
   const deleteProductMutation = useDeleteProductMutation();
   const addToWishlistMutation = useAddToWishlistMutation();
   const deleteFromWishlistMutation = useDeleteFromWishlistMutation();
-  const { _id, name, desc, price, banner, available, type } = product;
-
+  const { _id, name, desc, price, imageData, quantity, type } = product;
+  const available = quantity > 0 ? true : false;
+  const imageBuffer = Buffer.from(imageData[0].data);
+  const banner = `data:${imageData.contentType};base64,${imageBuffer.toString(
+    'base64'
+  )}`;
 
   const handleChangeImageFormat = (e) => {
     e.preventDefault();
@@ -39,13 +49,13 @@ export const ProductCard = ({ product, wishlistIdArray}) => {
 
   const handleAddToWishlist = (e) => {
     e.preventDefault();
-    addToWishlistMutation.mutate({product_id: product._id});
-  }
+    addToWishlistMutation.mutate({ product_id: product._id });
+  };
 
   const handleRemoveFromWishlist = (e) => {
     e.preventDefault();
-    deleteFromWishlistMutation.mutate({id: product._id});
-  }
+    deleteFromWishlistMutation.mutate({ id: product._id });
+  };
 
   const handleDelete = (e) => {
     e.preventDefault();
@@ -66,24 +76,45 @@ export const ProductCard = ({ product, wishlistIdArray}) => {
             }}
           >
             <ImageSwitchIconContainer>
-            <CgArrowsExchange
-              size={20}
-              onClick={handleChangeImageFormat}
-            /></ImageSwitchIconContainer>
+              <CgArrowsExchange size={20} onClick={handleChangeImageFormat} />
+            </ImageSwitchIconContainer>
           </CardImage>
-        </a>{available?<AvailabilityTag >
-          <strong>Available</strong>
-        </AvailabilityTag>:<OutOfStockTag >
-          <strong>Out of Stock</strong>
-        </OutOfStockTag>}
-        
+        </a>
+        {available ? (
+          <AvailabilityTag>
+            <strong>Available</strong>
+          </AvailabilityTag>
+        ) : (
+          <OutOfStockTag>
+            <strong>Out of Stock</strong>
+          </OutOfStockTag>
+        )}
+
         <CardContentContainer>
-        {authData? authData?.role === 'seller'? "" : wishlistIdArray?.includes(product._id)?<HeartIcon>
-          <AiFillHeart size={30} tw="text-red-500 hover:text-red-600" onClick={handleRemoveFromWishlist} />
-        </HeartIcon>:<HeartIcon>
-          <AiOutlineHeart size={30} tw="hover:text-red-600" onClick={handleAddToWishlist} />
-        </HeartIcon>: ""}
-        
+          {authData ? (
+            authData?.role === 'seller' ? (
+              ''
+            ) : wishlistIdArray?.includes(product._id) ? (
+              <HeartIcon>
+                <AiFillHeart
+                  size={30}
+                  tw="text-red-500 hover:text-red-600"
+                  onClick={handleRemoveFromWishlist}
+                />
+              </HeartIcon>
+            ) : (
+              <HeartIcon>
+                <AiOutlineHeart
+                  size={30}
+                  tw="hover:text-red-600"
+                  onClick={handleAddToWishlist}
+                />
+              </HeartIcon>
+            )
+          ) : (
+            ''
+          )}
+
           <a
             href={`/product-details/${_id}`}
             tw="text-lg font-semibold mb-2 block hover:text-indigo-600"
@@ -95,19 +126,24 @@ export const ProductCard = ({ product, wishlistIdArray}) => {
             <span tw="text-gray-500 text-sm">(${price})</span>
           </div>
         </CardContentContainer>
-        
+
         <div tw="flex items-center mt-2 space-x-2 justify-end p-2 relative">
-          {authData? authData?.role === 'seller'?<CardButton
-            tw="bg-red-500 hover:bg-red-600"
-            onClick={handleDelete}
-          >
-            Delete
-          </CardButton>:<CardButton
-            tw="bg-blue-500 hover:bg-blue-600"
-            
-          >
-            Add to Cart
-          </CardButton>:""}
+          {authData ? (
+            authData?.role === 'seller' ? (
+              <CardButton
+                tw="bg-red-500 hover:bg-red-600"
+                onClick={handleDelete}
+              >
+                Delete
+              </CardButton>
+            ) : (
+              <CardButton tw="bg-blue-500 hover:bg-blue-600">
+                Add to Cart
+              </CardButton>
+            )
+          ) : (
+            ''
+          )}
         </div>
       </CardContainer>
     </>
--- client/src/queries/product-queries.js
+++ client/src/queries/product-queries.js
@@ -5,17 +5,33 @@ export function useCreateProductMutation() {
   const queryClient = useQueryClient();
 
   const createProductMutation = useMutation(
-    async ({ name, desc, type, banner, unit, price, available, suplier }) => {
-      const response = await PostData('products/create', {
-        name,
-        desc,
-        type,
-        banner,
-        unit,
-        price,
-        available,
-        suplier,
-      });
+    async ({ name, desc, type, imageData, unit, price, available, active }) => {
+      // Create a FormData object and append the file
+      let formData = new FormData();
+      formData.append('user_id', 'dummy_id');
+      formData.append('name', name);
+      formData.append('description', desc);
+      formData.append('category_type', type);
+      formData.append('quantity', unit);
+      formData.append('price', price);
+      formData.append('active', active);
+      formData.append('imageData', imageData);
+      // Check if imageData is a single file or a FileList
+      if (imageData instanceof FileList) {
+        // If it's a FileList, append all files under the same key
+        for (let i = 0; i < imageData.length; i++) {
+          formData.append('imageData', imageData[i]);
+        }
+      } else if (imageData instanceof File) {
+        // If it's a single File, append it directly under the key "imageData"
+        formData.append('imageData', imageData);
+      }
+      // to log the form data
+      // for (const [key, value] of formData.entries()) {
+      //   console.log(`${key}: ${value}`);
+      // }
+
+      const response = await PostData('product/create', formData);
       return response.data;
     },
     {
