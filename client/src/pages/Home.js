b0ad306518e3de6727565c6d5df756fe0eca9fe7
aswerty123 <chirpy_melody@hotmail.com>
2023-10-20 08:11:04 +0800

    change style of landing and home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,15 +3,24 @@ import React from 'react';
 /** @jsxImportSource @emotion/react */
 import tw from 'twin.macro';
 
-const Heading = tw.h1`text-blue-500 text-2xl p-2`;
-const Container = tw.div`max-w-4xl mx-auto p-5 mt-5`;
+const Heading = tw.h1`text-blue-500 font-bold text-2xl p-2`;
+// const Container = tw.div`max-w-4xl mx-auto p-5 mt-5`;
 
 export const Home = () => {
+  const containerStyle = {
+    // backgroundColor: '#FFC0CB', // Set the background color to red
+    backgroundColor: '#FFFFFF', // Set the background color to red
+    height: '90vh', // Set the height to 100% of the viewport height
+    width: '100vw', // Set the width to 100% of the viewport width
+    display: 'flex',
+    justifyContent: 'center',
+    // alignItems: 'center',
+  };
   return (
     <>
-      <Container>
+      <div style={containerStyle}>
         <Heading>This is Protected HomePage</Heading>
-      </Container>
+      </div>
     </>
   );
 };
--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -4,14 +4,23 @@ import React from 'react';
 import tw from 'twin.macro';
 
 const Heading = tw.h1`text-blue-500 text-2xl p-2`;
-const Container = tw.div`max-w-4xl mx-auto p-5 mt-5`;
+// const Container = tw.div`max-w-4xl mx-auto p-5 mt-5`;
 
 export const Landing = () => {
+  const containerStyle = {
+    // backgroundColor: '#FFC0CB', // Set the background color to red
+    backgroundColor: '#FFFFFF', // Set the background color to red
+    height: '90vh', // Set the height to 100% of the viewport height
+    width: '100vw', // Set the width to 100% of the viewport width
+    display: 'flex',
+    justifyContent: 'center',
+    // alignItems: 'center',
+  };
   return (
     <>
-      <Container>
+      <div style={containerStyle}>
         <Heading>This is LandingPage</Heading>
-      </Container>
+      </div>
     </>
   );
 };
